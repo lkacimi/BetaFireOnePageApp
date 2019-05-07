9d53210f4ac1bb0522b08b250fa55fe0ac00fb07
Lamine Kacimi <kacimi@amazon.com>
2019-05-07 16:06:01 -0700

    adds frontend routing

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import BetaFireApp from './components/BetaFireApp';
 
-ReactDOM.render(<BetaFireApp />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><BetaFireApp /></BrowserRouter>, document.getElementById('root'));
